3c29c82ca01b5492f32979fbf5fa1c3e143c2a18
dathanh <datthanh304@gmail.com>
2018-07-24 18:07:29 +0700

    updta

---
+++ js/template.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     var index = 0;
+
+//     function displayBanner() {
+//         // $('banner-item').css({ display: 'none' });
+//         $('banner-item')[index].css({ display: 'block' });
+//         index++;
+//         setTimeout(displayBanner, 100);
+//     }
+// });
