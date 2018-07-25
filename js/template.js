187a132c89d008041d2aeb5579b0100edfe90ded
dathanh <datthanh304@gmail.com>
2018-07-25 18:04:28 +0700

    update

--- js/template.js
+++ js/template.js
@@ -1,10 +1,20 @@
-// $(document).ready(function() {
-//     var index = 0;
+$(document).ready(function() {
+    // var index = 0;
 
-//     function displayBanner() {
-//         // $('banner-item').css({ display: 'none' });
-//         $('banner-item')[index].css({ display: 'block' });
-//         index++;
-//         setTimeout(displayBanner, 100);
-//     }
-// });
+    // function displayBanner() {
+    //     // $('banner-item').css({ display: 'none' });
+    //     $('banner-item')[index].css({ display: 'block' });
+    //     index++;
+    //     setTimeout(displayBanner, 100);
+    // }
+    $("div.mobile-menu>a:first-child").on('click', function() {
+        if ($("div.menu-content").css('display') == 'none') {
+            $("div.menu-content").css({ display: 'block' });
+            $("div.mobile-menu>a:first-child").css({ "background-color" : '#4caf50' });
+        } else {
+            $("div.menu-content").css({ display: 'none' });
+            $("div.mobile-menu>a:first-child").css({ "background-color" : '#0F0E0E' });
+        }
+
+    });
+});
